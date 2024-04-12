5c5cd493174df2eeafefd19ae6870c576f9a0eff
Tim Kwok <timkwok808@gmail.com>
2024-04-12 03:31:44 -0700

    added sizing and able to click to color, need to add funcitonality to other color buttons

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = "repeat(16 , 1fr)"
-board.style.gridTemplateRows = "(repeat(16, 1fr)"
+let color = "black"
 
-for(let i = 0; i <256; i++){
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
-    board.insertAdjacentElement("beforeend", square);
+function populateBoard(size) {
+    let board = document.querySelector('.board')
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`
+
+    let amount = size * size
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div')
+        square.addEventListener('click', colorSquare)
+        square.style.backgroundColor = 'blue'
+        board.insertAdjacentElement("beforeend", square);
+    }
+
+}
+
+populateBoard(16)
+
+function changeSize(input){
+    populateBoard(input)
+}
+
+function colorSquare(){
+    this.style.backgroundColor = "color";
 }
